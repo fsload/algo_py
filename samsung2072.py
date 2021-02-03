bb2779d7d20e95adde317d7d691cf55d1f64d6c7
fsload <60029949+fsload@users.noreply.github.com>
2021-02-03 20:42:08 +0900

    deL

--- samsung2072.py
+++
@@ -1,9 +0,0 @@
-TC = int(input())
-for test_case in range(1, TC+1) :
-    nums = input().split()
-    nums = list(map(int,nums))
-    total = [0 for i in range(TC+1)]
-    for i in range(10):
-        if nums[i]%2 != 0:
-            total[test_case] += nums[i]
-    print("#%d %d" %(test_case, total[test_case]))
