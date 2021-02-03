4b3a1c516cb28eea69f6623adae8579c7a38ebc7
fsload <60029949+fsload@users.noreply.github.com>
2021-02-04 05:25:31 +0900

    sssaaaffyyy

--- day4/selection2.py
+++ day4/selection2.py
@@ -3,13 +3,14 @@ li = []
 
 def sort(li,i):
     while i<100:
+        print(i)
         a = min(li[i:])
         idx = li.index(a)
         tmp = li[i]
         li[i] = li[idx]
         li[idx] = tmp
         i += 1
-        return sort(li,i)
+        sort(li,i)
 
 while True:
     i = random.randint(1, 100)
@@ -23,3 +24,22 @@ i = 0
 
 sort(li,i)
 print('after sort : ', li)
+
+
+
+
+func(list , curIdx)
+
+
+func([4,2,1,5,3], 0)
+|
+func([1,2,4,5,3], 1)
+|
+func()
+|
+func([1,2,3,4,5]. 4) ==> if curIdx == len - 1
+
+
+func([0 .. 1], 0)  --- func([0 .. 1] 1) ------------------ func()
+                                         ----------------- func(78)
+                   --- func([], 2)
